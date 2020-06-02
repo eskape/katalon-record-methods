838f03a459981b6f2bb503a8d03d1e09a86d0aea
Kishen Simbhoedatpanday <eskapenl@gmail.com>
2020-06-02 17:08:34 +0200

    Adds running in Docker

---
+++ Test Suites/TS_RegressionTest.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_RegressionTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>792e52c8-a285-48e5-8dbb-91f958d49aa9</testSuiteGuid>
+   <testCaseLink>
+      <guid>681edf61-8730-44f0-a810-677d7409f19c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Healthcare Service Appointment</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4f34246e-1509-462e-b479-da3dc11f36dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BudgetCalculator</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ runDockerChrome.sh
@@ -0,0 +1,3 @@
+set -xe
+
+docker run -t --rm -v "$(pwd)":/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"
