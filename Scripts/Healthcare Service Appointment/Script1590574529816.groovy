85d538757c5942c526758a53564df9b9b037c293
Kishen Simbhoedatpanday <eskapenl@gmail.com>
2020-06-18 16:45:07 +0200

    Fail fast

--- Scripts/Healthcare Service Appointment/Script1590574529816.groovy
+++ Scripts/Healthcare Service Appointment/Script1590574529816.groovy
@@ -20,6 +20,8 @@ WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
 
 WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'))
 
+assert false
+
 WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Username_username'), 'John Doe')
 
 WebUI.setEncryptedText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Password_password'), 'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
